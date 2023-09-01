932caf33d1cc49b6c66512edf669ef943065529f
Krane <56824280+freesme@users.noreply.github.com>
2023-09-01 23:08:22 +0800

    Delete world_hello/target directory

--- world_hello/target/debug/deps/world_hello-837f871c63f12f2a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\CatBread\RustProject\hello_rust\world_hello\target\debug\deps\world_hello-837f871c63f12f2a.rmeta: src\main.rs
-
-C:\Users\CatBread\RustProject\hello_rust\world_hello\target\debug\deps\world_hello-837f871c63f12f2a.d: src\main.rs
-
-src\main.rs:
